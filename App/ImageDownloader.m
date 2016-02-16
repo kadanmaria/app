d9250098213ec2030a85eb98074a90db00805538
Admin <admin@Mac-Admin.local>
2016-02-16 20:51:56 +0200

    NSURLSession

--- App/ImageDownloader.m
+++ App/ImageDownloader.m
@@ -8,10 +8,8 @@
 
 #import "ImageDownloader.h"
 
-@interface ImageDownloader () <NSURLConnectionDelegate>
+@interface ImageDownloader ()
 
-@property (strong, nonatomic) NSURLConnection *currentConnection;
-@property (strong, nonatomic) NSMutableData *dataContainingImage;
 @property (assign, nonatomic) NSIndexPath *indexPath;
 @property (strong, nonatomic) UIImage *image;
 
@@ -24,40 +22,25 @@
     NSString *restCall = imageString;
     NSURL *restURL = [NSURL URLWithString:restCall];
     NSURLRequest *restReqest = [NSURLRequest requestWithURL:restURL];
-    if (self.currentConnection) {
-        [self.currentConnection cancel];
-        self.currentConnection = nil;
-        self.dataContainingImage = nil;
-    }
-    self.currentConnection = [[NSURLConnection alloc] initWithRequest:restReqest delegate:self];
-}
-
-#pragma mark - <NSURLConnectionDelegate>
-
-- (void)connection:(NSURLConnection*)connection didReceiveResponse:(NSURLResponse *)response {
-    self.dataContainingImage = [NSMutableData data];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    [self.dataContainingImage appendData:data];
-}
-
-- (void)connection:(NSURLConnection*)connection didFailWithError:(NSError*)error {
-    NSLog(@"URL Connection Failed With Error %@!", error);
-    self.currentConnection = nil;
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    UIImage *image = [UIImage imageWithData:self.dataContainingImage];
-        if (!image) {
-            NSLog(@"Error Setting Image");
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *imageData = [session dataTaskWithRequest:restReqest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if(!error) {
+        UIImage *image = [UIImage imageWithData:data];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (!image) {
+                NSLog(@"Error Setting Image");
+            } else {
+                self.image = image;
+                [self.delegate imageDownloader:self didDownloadImage:self.image forIndexPath:self.indexPath];
+                
+            }
+        });
         } else {
-            self.image = image;
-//            NSLog(@"Did download image for row %lu", self.indexPath.row);
-            [self.delegate imageDownloader:self didDownloadImage:self.image forIndexPath:self.indexPath];
-            
+            NSLog(@"Error imageDataTask %@", error);
         }
-    self.currentConnection = nil;
+    }];
+    [imageData resume];
 }
 
 @end
